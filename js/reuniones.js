c3386689dc3b2ed57a6cdd2f34362593d6427955
leoachear <leoachear@gmail.com>
2021-09-08 18:54:37 -0300

    Version final

--- js/reuniones.js
+++ js/reuniones.js
@@ -1,6 +1,7 @@
 const listMeeting = document.getElementById('reuniones');
 
 listMeeting.innerHTML = /*html*/`<h4>Mis reuniones</h4>
+    <div class="line"></div>
     <select name="select">
         <option class="select-item" value="Agosto">Agosto</option>
         <option class="select-item" value="Septiembre" selected>Septiembre</option>
@@ -14,9 +15,10 @@ listMeeting.innerHTML = /*html*/`<h4>Mis reuniones</h4>
                 <div class="meeting-item-desc">
                     <h5 class="meeting-item-title">Jam</h5>
                     <p>Lluvia de ideas para Luxi</p>
-                </div>
-                <div class="meeting-item-obs">
                     <p>Duración: 1.5 hrs aprox.</p>
+                </div>
+                <div class="line-vertical"></div>
+                <div class="meeting-item-obs">
                     <p>Observaciones: Por zoom</p>
                 </div>
                 </div>
@@ -26,9 +28,31 @@ listMeeting.innerHTML = /*html*/`<h4>Mis reuniones</h4>
                 </div>
             </div>
         </div>
+        <div class="meeting-item" id="meeting-item">
+            <p class="meeting-item-date">17 - septiembre, 2021</p>
+            <div class="meeting-item-container">
+                <div class="meeting-item-info">
+                <div class="meeting-item-desc">
+                    <h5 class="meeting-item-title">Jam</h5>
+                    <p>Lluvia de ideas para Luxi</p>
+                    <p>Duración: 1.5 hrs aprox.</p>
+                </div>
+                <div class="line-vertical"></div>
+                <div class="meeting-item-obs">
+                    <p>Observaciones: Por zoom</p>
+                </div>
+                </div>
+                <div class="meeting-item-actions">
+                    <p class="votes">4 <br>votos</p>
+                    <a id="more" href=""><img src="../assets/img/arrow.svg" alt=""></a>
+                </div>
+            </div>
+        </div>
     </div>
-    <button class="btn-secondary">Nueva Reunión</button>
-    <button class="btn-empty">Mi Perfil</button>`;
+    <div class="btn-container">
+        <button class="btn-secondary">Nueva Reunión</button>
+        <button class="btn-empty">Mi Perfil</button>
+    </div>`;
 
 /******************************/
 
