6c9b61b038139a3876b9c08c2316d385c54b4b97
DamianPalavecino <damianpalavecino@gmail.com>
2021-09-14 21:30:02 -0300

    Agrego boton para mostrar/ocultar password

--- js/registro.js
+++ js/registro.js
@@ -10,12 +10,33 @@ formularioRegistro.innerHTML = /* html */ `
     <label class="register-label">Correo electrónico:</label>
     <input type="text" class="register-input">
     <label class="register-label">Contraseña:</label>
-    <input type="password" class="register-input">
+    <span class="password-reveal">
+    <i class="far fa-eye"></i>
+    <i class="far fa-eye-slash hide"></i>
+    </span>
+    <input type="password" class="register-input" id="password-input">
     <label class="register-label">Ciudad, País:</label>
     <input type="text" class="register-input">
     </div>
     <button class="form-button">Regístrate</button>
     <p class="form-footer">Ya tenés una cuenta? <a href="#" class="to-login">Login</a>
   </form> 
-  
+
 `;
+
+const passwordReveal = document.querySelector(".password-reveal")
+passwordReveal.addEventListener('click', () => {
+  const passwordInput = document.querySelector('#password-input');
+  const showPassword = document.querySelector('.fa-eye');
+  const hidePassword = document.querySelector('.fa-eye-slash');
+  if (passwordInput.type === 'password') {
+    showPassword.classList.add('hide');
+    hidePassword.classList.remove('hide');
+    passwordInput.type = 'text'
+  } 
+  else {
+    showPassword.classList.remove('hide');
+    hidePassword.classList.add('hide');
+    passwordInput.type = 'password';
+  }
+});
