99e0d1046a1d768ebd3ff9852bdc0bbb18300638
luciabrumnich <luciabrumnichhr@gmail.com>
2021-09-28 08:36:06 -0300

    se elimino el archivo primeros-pasos.js

--- js/primeros-pasos.js
+++
@@ -1,9 +0,0 @@
-const menu = document.querySelector('#menu');
-const nav = document.querySelector('.nav');
-//const list = document.querySelectorAll('.menu li');
-
-menu.addEventListener('click', function (e) {
-  console.log('menu');
-  e.preventDefault();
-  nav.classList.toggle('visible');
-});
