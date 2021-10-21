844028e041a453098f4980f0a06496dfcf6468b3
leoachear <leoachear@gmail.com>
2021-10-21 07:26:51 -0300

    Se agregan los datos del requisito del Issue, y aparte otros más.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,20 @@
+import React from 'react';
+import { CardMeeting } from '../components/CardMeeting/CardMeeting'
+
 export function Home() {
-  return <p>Hola mango!</p>;
+  return (
+    <>
+      <p>Hola mango!</p>
+      <CardMeeting 
+        dateTime="17 - septiembre, 2021"
+        title="Reunión de ideas"
+        description="Discutir las ideas del proyecto"
+        observations="Por zoom"
+        duration="2hrs aprox."
+        votes="2"
+        voteUrl=""
+        detailsUrl=""
+      />
+    </>
+  );
 }
