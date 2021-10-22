ac266cbae3ade3133f10dbceb5e74bcc45f19cf3
leoachear <leoachear@gmail.com>
2021-10-22 11:39:04 -0300

    Usando props.children.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,14 @@
+import React from 'react';
+
+import { Message } from '../components/Message/Message';
+
 export function Home() {
-  return <p>Hola mango!</p>;
+  return (
+    <>
+      <p>Hola mango!</p>
+      <Message>
+        <p> Ahora tus invitades tienen que elegir el mejor horario. Podés ver las votaciones en <span>“Mis reuniones”</span></p>
+      </Message>
+    </>
+  );
 }
