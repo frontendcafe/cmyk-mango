56dee655960a03e0ec36b4688abe36ec2d2b6f46
Lucia Nina <lucianina1994@gmail.com>
2021-11-02 18:17:09 -0300

    Se agrego ButtonComponent

---
+++ src/components/ButtonComponent/ButtonComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Button.module.css';
+
+const ButtonComponent = ({ name }) => {
+  return (
+       <button className={styles.button} >{name}</button>
+  );
+};
+
+export default ButtonComponent;
