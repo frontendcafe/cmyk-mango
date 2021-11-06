bec3e13ccedd3775a5937ec7ae49e38ffad30c70
leoachear <leoachear@gmail.com>
2021-11-06 08:24:10 -0300

    Resueltos los pedidos de cambios hasta el 06/11

--- src/components/MobileNavbar/MobileNavbar.jsx
+++ src/components/MobileNavbar/MobileNavbar.jsx
@@ -1,34 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from './MobileNavbar.module.css';
 import newMeeting from '../../assets/images/my-meetings.svg';
-import perfil from '../../assets/images/perfil.svg';
+import profile from '../../assets/images/profile.svg';
 import myMeetings from '../../assets/images/my-meetings.svg';
 
 function MobileNavbar() {
     return(
-        <div className={ styles["footer"] }>
-            <div className={ styles["icons-footer" ]}>
-                <div>
-                    <a id="new-meeting" href="">
+        <footer>
+            <div className={ styles["icons-footer"] }>
+                <Link 
+                    className={ styles["navbar-button"] }
+                    to=""
+                    target=""
+                >
+                    <div>
                         <img src={newMeeting} alt="Nueva reunión"/>
-                    </a>
-                    <p>Nueva reunión</p>
-                </div>
-                <div>
-                    <a id="perfil" href="">
-                        <img src={perfil} alt="Perfil"/>
-                    </a>
-                    <p>Perfil</p>
-                </div>
-                <div>
-                    <a id="my-meetings" href="">
+                        <p>Nueva reunión</p>
+                    </div>
+                </Link>
+                <Link 
+                    className={ styles["navbar-button"] }
+                    to=""
+                    target=""
+                >
+                    <div>
+                        <img src={profile} alt="Perfil"/>
+                        <p>Perfil</p>
+                    </div>
+                </Link>
+                <Link 
+                    className={ styles["navbar-button"] }
+                    to=""
+                    target=""
+                >
+                    <div>
                         <img src={myMeetings} alt="Mis reuniones"/>
-                    </a>
-                    <p>Mis reuniones</p>
-                </div>
+                        <p>Mis reuniones</p>
+                    </div>
+                </Link>
             </div>
-        </div>
+        </footer>
     );
 }
 
