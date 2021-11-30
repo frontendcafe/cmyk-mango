d40a61b0550c0aafc9ef6656f1935ed0ddb4b20f
Ismael Morel <ismaelmorel84@gmail.com>
2021-11-29 21:38:56 -0300

    Config fb/prettier added

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+
+//Firebase Config utilizando variables de entorno
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+};
+
+//Inicializamos app, db, auth, authGoogle
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+
+//Exportamos
+export { db, auth, googleProvider };
