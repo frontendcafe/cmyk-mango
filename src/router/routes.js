9532341826cbdc738ffc21b38cce9284bf720740
Ismael Morel <ismaelmorel84@gmail.com>
2021-12-01 18:11:49 -0300

    Fix 2 homes, fix prettier

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,8 @@
 import { Home } from '../pages/Home/Home';
-import { Home2 } from '../pages/Home2/Home2';
 
 export const routes = [
   {
-    path: '/home2',
-    component: Home,
-  },
-  {
     path: '/',
-    component: Home2,
+    component: Home,
   },
 ];
