7c600c5f94c7ee3986be58241a5f3ddea1405aa5
Jonatan Luis Moreno <jonatanluismoreno@gmail.com>
2021-12-06 02:45:28 -0300

    fix visual issues

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,15 @@
 import { Home } from '../pages/Home/Home';
 import { Home2 } from '../pages/Home2/Home2';
+import { SignIn } from '../../src/pages/SignIn/SignIn';
 
 export const routes = [
   {
     path: '/home2',
     component: Home2,
+  },
+  {
+    path: '/signin',
+    component: SignIn,
   },
   {
     path: '/',
