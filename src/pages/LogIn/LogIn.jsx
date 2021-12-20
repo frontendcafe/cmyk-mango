81a3ea18d06dfc650f4789b2eec362cdf99588a5
Jonatan Luis Moreno <jonatanluismoreno@gmail.com>
2021-12-20 16:01:52 -0300

    signin basics

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { ButtonComponent } from '../../components/ButtonComponent/ButtonComponent';
 import { Input } from '../../components/Input/Input';
-import styles from './SignIn.module.css'
+import styles from './LogIn.module.css'
 
 import mangoimage from '../../assets/images/mango.svg'
 
-export function SignIn() {
+export function LogIn() {
   return (
     <div className={styles.signin}>
       <header className={styles.header}>Mango</header>
@@ -14,31 +14,25 @@ export function SignIn() {
         <h5 className={styles.mangotitle}>MANGO</h5>
       </section>
       <form className={styles.formsection}>
-        <div>
-          <h2 className={styles.formtitle}>Bienvenidx a Mango</h2>
-          <h3 className={styles.formdata}>Nombre y apellido</h3>
-          <Input/>
-          <h3 className={styles.formdata}>Correo electrónico</h3>
-          <Input/>
-          <h3 className={styles.formdata}>Contraseña</h3>
-          <Input/>
-          <h3 className={styles.formdata}>Ciudad, País</h3>
-          <Input/>
-        </div>
-          <a className={styles.button} href="/">
-            <ButtonComponent name="Registrate"/>
-          </a>
+        <h2 className={styles.formtitle}>¡Bienvenidx!</h2>
+        <h3 className={styles.formsubtitle}>Ingresa a tu cuenta</h3>
+        <Input placeholder="Correo electrónico"/>
+        <Input placeholder="Contraseña"/>
+        <a href="/forgetpass">
+          <h4 className={styles.forgetpass}>Olvidaste tu contraseña?</h4>
+        </a>
+        <a href="/">
+          <ButtonComponent name="Log in"/>
+        </a>
         <h4 className={styles.haveacc}>
-          Ya tenés una cuenta?  
+          ¿No tenés cuenta?  
           <a href="/login">
             <h5 className={styles.createacc}>
-              Login
-            </h5> 
+              Crear cuenta
+            </h5>
           </a>
         </h4>
-        
       </form>
     </div>
   );
 }
-
