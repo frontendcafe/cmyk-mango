64d995a99d0b8b7fb119c7ab4dd83b53fe23e290
Jonatan Luis Moreno <jonatanluismoreno@gmail.com>
2022-02-02 19:45:48 -0300

    minimal fixes

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -6,7 +6,7 @@ function Input(props) {
   return (
     <label className={styles.label}>
       {props.text}
-      <input type={props.typeName} placeholder={props.placeholder} className={styles.input} />
+      <input type={props.typeName} placeholder={props.placeholder} className={`${styles.input} ${props.bigger ? `${styles.bigger}` : ''}`} />
     </label>
   );
 }
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'
 import { ButtonComponent } from '../../components/ButtonComponent/ButtonComponent';
 import { Input } from '../../components/Input/Input';
 import styles from './SignIn.module.css'
 
 import mangoimage from '../../assets/images/mango.svg'
+import eye from '../../assets/images/showpassword.svg'
+import eyeSlash from '../../assets/images/showpass.svg'
 
 export function SignIn() {
+  const [showPass, setShowPass] = useState("password")
+
+  function togglePass() {
+    if (showPass === 'password') {
+      setShowPass('')
+    }
+    else {
+      setShowPass('password')
+    }
+  }
   return (
     <div className={styles.signin}>
       <header className={styles.header}>Mango</header>
@@ -14,27 +27,41 @@ export function SignIn() {
         <h5 className={styles.mangotitle}>MANGO</h5>
       </section>
       <form className={styles.formsection}>
-        <div>
+        <ul>
           <h2 className={styles.formtitle}>Bienvenidx a Mango</h2>
-          <h3 className={styles.formdata}>Nombre y apellido</h3>
-          <Input/>
-          <h3 className={styles.formdata}>Correo electrónico</h3>
-          <Input/>
-          <h3 className={styles.formdata}>Contraseña</h3>
-          <Input/>
-          <h3 className={styles.formdata}>Ciudad, País</h3>
-          <Input/>
-        </div>
-          <a className={styles.button} href="/">
-            <ButtonComponent name="Registrate"/>
-          </a>
+          <li className={styles.formdata}>
+            <h3 className={styles.datatitle}>Nombre y apellido</h3>
+            <Input bigger/>
+          </li>
+          <li className={styles.formdata}>
+            <h3 className={styles.datatitle}>Correo electrónico</h3>
+            <Input bigger/>
+          </li>
+          <li className={styles.formdata}>
+            <h3 className={styles.datatitle}>Contraseña</h3>
+            
+            <Input bigger typeName={showPass} />
+          </li>
+          
+          <li className={styles.formdata}>
+            <h3 className={styles.datatitle}>Ciudad, País</h3>
+            <Input bigger />
+          </li>
+        </ul>
+        <picture className={styles.passVisibility}>
+            <img src={eye} alt="showpass" onClick={togglePass} />
+            <img src={eyeSlash} alt="showpass" className={styles.eye} onClick={togglePass} />
+        </picture>
+        <a className={styles.button} href="/">
+          <ButtonComponent name="Registrate"/>
+        </a>
         <h4 className={styles.haveacc}>
           Ya tenés una cuenta?  
-          <a href="/login">
+          <Link to="/login">
             <h5 className={styles.createacc}>
               Login
             </h5> 
-          </a>
+          </Link>
         </h4>
         
       </form>
